8d345c8bcfc2bd837f7660c6e9428bccc2daef8e
YFROBOTZL <yfrobot-zl@foxmail.com>
2022-08-17 15:13:21 +0800

    V0.0.1

--- arduinoC/main.ts
+++ arduinoC/main.ts
@@ -7,81 +7,81 @@
  * @copyright    MIT Lesser General Public License
  * 
  * @author [email](yfrobot@qq.com)
- * @date  2022-05-07
+ * @date  2022-08-17
 */
 
-enum RTCMODULE {
-  //% block="PCF8563"
-  PCF8563,
-  //% block="DS1307"
-  DS1307,
-  //% block="DS3231"
-  DS3231
+enum SERIAL {
+    //% block="Hardware"
+    Hardware,
+    //% block="Software"
+    Software
 }
 
-enum RTCDATETIME {
-    //% block="Year"
-    year,
-    //% block="Month"
-    month,
-    //% block="Day"
-    day,
-    //% block="Hour"
-    hour,
-    //% block="Minute"
-    minute,
-    //% block="Second"
-    second,
-    //% block="Day of the week"
-    dayOfTheWeek
-  }
-
 //% color="#fecc11" iconWidth=40 iconHeight=40
-namespace rtc {
-  //% block="RTC [MODULE] initliallize" blockType="command"
-  //% MODULE.shadow="dropdown" MODULE.options="RTCMODULE" MODULE.defl="RTCMODULE.PCF8563"
-  export function init(parameter: any, block: any) {
-    // let intpin = parameter.INTPIN.code;
-    let module = parameter.MODULE.code;
+namespace vc {
+  
+  //% block="VC Hardware [SERIAL] recognize speech and receive data " blockType="reporter"
+  //% SERIAL.shadow="dropdown" SERIAL.options="SER"
+  export function vcSpeechRecognitionHardware(parameter: any, block: any) {
+    let serial = parameter.SERIAL.code;
+
+    Generator.addInclude("include_variable_inBuf", "uint8_t inBuf[5];  //数据数组");
+
+    Generator.addInclude("include_HardwareSerial", `#define yfrobotSerial ${serial}`);
     
-    Generator.addInclude("includeRTClib", "#include \"RTClib.h\"");
-    if (module === "PCF8563") {
-        Generator.addObject("rtcObject", "RTC_PCF8563", `rtc`);
-    } else if (module === "DS1307") {
-        Generator.addObject("rtcObject", "RTC_DS1307", `rtc`);
-    } else if (module === "DS3231") {
-        Generator.addObject("rtcObject", "RTC_DS3231", `rtc`);
-    }
-    Generator.addSetup(`initSetup`, `if (! rtc.begin()) {\n    while (1) delay(10);\n  }`);
+    Generator.addSetup(`vc_initSetup`, `yfrobotSerial.begin(115200);`);
+    Generator.addInclude(`defineserialReceiveData`, `PROGMEM uint8_t serialReceiveData();`)
+    Generator.addInclude(`defineserialReceiveDataFun`, ``+
+        `uint8_t serialReceiveData() {\n`+
+        `    int datalen = yfrobotSerial.available();\n`+
+        `    if (datalen >= 5) { // 接收到一组数据\n`+
+        `      while (datalen--) {\n`+
+        `        inBuf[4 - datalen] = yfrobotSerial.read();\n`+
+        `      }\n`+
+        `      return inBuf[1];\n`+
+        `    } else {\n`+
+        `      return 0x01;  // 默认未接收到数据时，返回1\n`+
+        `    }\n`+
+        `}`
+    );
+
+    Generator.addCode('serialReceiveData()');
   }
 
-  //% block="RTC set date and time this sketch was compiled" blockType="command"
-  export function setDateTimeCompiled(parameter: any, block: any) {
-    Generator.addCode("rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));");
-  }
-
-  //% block="RTC set date and time [YEAR]-[MONTH]-[DAY] [HOUR]:[MINUTE]:[SECOND]" blockType="command"
-  //% YEAR.shadow="number" YEAR.defl="2022"
-  //% MONTH.shadow="number" MONTH.defl="5"
-  //% DAY.shadow="number" DAY.defl="7"
-  //% HOUR.shadow="number" HOUR.defl="10"
-  //% MINUTE.shadow="number" MINUTE.defl="20"
-  //% SECOND.shadow="number" SECOND.defl="0"
-  export function setDateTime(parameter: any, block: any) {
-    let year = parameter.YEAR.code;
-    let month = parameter.MONTH.code;
-    let day = parameter.DAY.code;
-    let hour = parameter.HOUR.code;
-    let minute = parameter.MINUTE.code;
-    let second = parameter.SECOND.code;
     
-    Generator.addCode(`rtc.adjust(DateTime(${year}, ${month}, ${day}, ${hour}, ${minute}, ${second}));`);
+  //% block="VC Software RX[rx]TX[tx] recognize speech and receive data " blockType="reporter"
+  //% rx.shadow="dropdown" rx.options="SSRXD"
+  //% tx.shadow="dropdown" tx.options="SSTXD"
+  export function vcSpeechRecognitionSoftware(parameter: any, block: any) {
+    let rxpin = parameter.rx.code;
+    let txpin = parameter.tx.code;
+
+    Generator.addInclude("include_variable_inBuf", "uint8_t inBuf[5];  //数据数组");
+
+    Generator.addInclude("include_SoftwareSerial", `#include <SoftwareSerial.h>\n`+
+        `SoftwareSerial mySerial(${rxpin}, ${txpin}); // RX, TX\n`+
+        `#define yfrobotSerial mySerial\n`
+    );
+    
+
+    Generator.addSetup(`vc_initSetup`, `yfrobotSerial.begin(115200);`);
+    Generator.addInclude(`defineserialReceiveData`, `PROGMEM uint8_t serialReceiveData();`)
+    Generator.addInclude(`defineserialReceiveDataFun`, ``+
+        `uint8_t serialReceiveData() {\n`+
+        `    int datalen = yfrobotSerial.available();\n`+
+        `    if (datalen >= 5) { // 接收到一组数据\n`+
+        `      while (datalen--) {\n`+
+        `        inBuf[4 - datalen] = yfrobotSerial.read();\n`+
+        `      }\n`+
+        `      return inBuf[1];\n`+
+        `    } else {\n`+
+        `      return 0x01;  // 默认未接收到数据时，返回1\n`+
+        `    }\n`+
+        `}`
+    );
+
+    Generator.addCode('serialReceiveData()');
+
   }
 
-  //% block="RTC get date and time [DATETIME]" blockType="reporter"
-  //% DATETIME.shadow="dropdown" DATETIME.options="RTCDATETIME" DATETIME.defl="RTCDATETIME.year"
-  export function getDateTime(parameter: any, block: any) {
-    let datetime = parameter.DATETIME.code;
-    Generator.addCode(`rtc.now().${datetime}()`);
-  }
 }
