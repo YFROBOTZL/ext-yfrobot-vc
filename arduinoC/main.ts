ceb8bbc59ee665f4aafb48c8eb4e3c60bc2c2af6
YFROBOTZL <yfrobot-zl@foxmail.com>
2023-03-04 11:35:00 +0800

    V0.0.2
    优化串口通信算法

--- arduinoC/main.ts
+++ arduinoC/main.ts
@@ -12,29 +12,42 @@
 
 //% color="#ea8958" iconWidth=40 iconHeight=40
 namespace vc {
-  
+    
   //% block="VC Hardware [SERIAL] recognize speech and receive data " blockType="reporter"
   //% SERIAL.shadow="dropdown" SERIAL.options="SER"
   export function vcSpeechRecognitionHardware(parameter: any, block: any) {
     let serial = parameter.SERIAL.code;
-
-    Generator.addInclude("include_variable_inBuf", "uint8_t inBuf[5];  //数据数组");
-
     Generator.addInclude("include_HardwareSerial", `#define yfrobotSerial ${serial}`);
     
     Generator.addSetup(`vc_initSetup`, `yfrobotSerial.begin(115200);`);
-    Generator.addInclude(`defineserialReceiveData`, `PROGMEM uint8_t serialReceiveData();`)
+    Generator.addInclude(`defineserialReceiveData`, `uint8_t serialReceiveData();`)
     Generator.addInclude(`defineserialReceiveDataFun`, ``+
         `uint8_t serialReceiveData() {\n`+
-        `    int datalen = yfrobotSerial.available();\n`+
-        `    if (datalen >= 5) { // 接收到一组数据\n`+
-        `      while (datalen--) {\n`+
-        `        inBuf[4 - datalen] = yfrobotSerial.read();\n`+
+        `  uint8_t c;\n`+
+        `  uint8_t inBuf[3];   //数据数组\n`+
+        `  static uint8_t offset;\n`+
+        `  static uint32_t checksum;\n`+
+        `  static enum _serial_state { IDLE, HEADER_5A } c_state;\n\n`+
+        `  while (yfrobotSerial.available()) {\n`+
+        `    c = yfrobotSerial.read();     // 读串口缓冲区\n`+
+        `    if (c_state == IDLE) {        // 串口状态空闲,等待 HEADER 5A 状态的到来\n`+
+        `      if (c == 0x5A) {            // 判断是否进入 HEADER 5A\n`+
+        `        checksum = c;             // 校验和 1  -  head\n`+
+        `        c_state = HEADER_5A;      // 进入 HEADER_5A 状态\n`+
+        `        offset = 0;\n`+
+        `      } else  c_state = IDLE;     // 返回IDLE状态\n`+
+        `    } else if (c_state == HEADER_5A && offset < 3) {\n`+
+        `      checksum += c;              // 校验和 2  -  data\n`+
+        `      inBuf[offset] = c;\n`+
+        `      offset++;\n`+
+        `    } else if (c_state == HEADER_5A && offset >= 3) {\n`+
+        `      c_state = IDLE;             // 返回IDLE状态\n`+
+        `      if ((checksum & 0xFF) == c) {\n`+
+        `        return inBuf[0];          // 返回指令值\n`+
         `      }\n`+
-        `      return inBuf[1];\n`+
-        `    } else {\n`+
-        `      return 0x01;  // 默认未接收到数据时，返回1\n`+
         `    }\n`+
+        `  }\n`+
+        `  return 0x01;                    // 未接收数据，默认返回0x01\n`+
         `}`
     );
 
@@ -49,27 +62,40 @@ namespace vc {
     let rxpin = parameter.rx.code;
     let txpin = parameter.tx.code;
 
-    Generator.addInclude("include_variable_inBuf", "uint8_t inBuf[5];  //数据数组");
-
     Generator.addInclude("include_SoftwareSerial", `#include <SoftwareSerial.h>\n`+
         `SoftwareSerial mySerial(${rxpin}, ${txpin}); // RX, TX\n`+
         `#define yfrobotSerial mySerial\n`
     );
     
-
     Generator.addSetup(`vc_initSetup`, `yfrobotSerial.begin(115200);`);
-    Generator.addInclude(`defineserialReceiveData`, `PROGMEM uint8_t serialReceiveData();`)
+    Generator.addInclude(`defineserialReceiveData`, `uint8_t serialReceiveData();`)
     Generator.addInclude(`defineserialReceiveDataFun`, ``+
         `uint8_t serialReceiveData() {\n`+
-        `    int datalen = yfrobotSerial.available();\n`+
-        `    if (datalen >= 5) { // 接收到一组数据\n`+
-        `      while (datalen--) {\n`+
-        `        inBuf[4 - datalen] = yfrobotSerial.read();\n`+
+        `  uint8_t c;\n`+
+        `  uint8_t inBuf[3];   //数据数组\n`+
+        `  static uint8_t offset;\n`+
+        `  static uint32_t checksum;\n`+
+        `  static enum _serial_state { IDLE, HEADER_5A } c_state;\n\n`+
+        `  while (yfrobotSerial.available()) {\n`+
+        `    c = yfrobotSerial.read();     // 读串口缓冲区\n`+
+        `    if (c_state == IDLE) {        // 串口状态空闲,等待 HEADER 5A 状态的到来\n`+
+        `      if (c == 0x5A) {            // 判断是否进入 HEADER 5A\n`+
+        `        checksum = c;             // 校验和 1  -  head\n`+
+        `        c_state = HEADER_5A;      // 进入 HEADER_5A 状态\n`+
+        `        offset = 0;\n`+
+        `      } else  c_state = IDLE;     // 返回IDLE状态\n`+
+        `    } else if (c_state == HEADER_5A && offset < 3) {\n`+
+        `      checksum += c;              // 校验和 2  -  data\n`+
+        `      inBuf[offset] = c;\n`+
+        `      offset++;\n`+
+        `    } else if (c_state == HEADER_5A && offset >= 3) {\n`+
+        `      c_state = IDLE;             // 返回IDLE状态\n`+
+        `      if ((checksum & 0xFF) == c) {\n`+
+        `        return inBuf[0];          // 返回指令值\n`+
         `      }\n`+
-        `      return inBuf[1];\n`+
-        `    } else {\n`+
-        `      return 0x01;  // 默认未接收到数据时，返回1\n`+
         `    }\n`+
+        `  }\n`+
+        `  return 0x01;                    // 未接收数据，默认返回0x01\n`+
         `}`
     );
 
